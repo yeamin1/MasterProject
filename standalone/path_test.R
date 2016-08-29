3a7e6214d368ed92a74cafb6b7621e58ecc6c289
jason <374567155@qq.com>
2016-08-30 10:49:46 +1200

    delete some useless script

--- standalone/path_test.R
+++
@@ -1,9 +0,0 @@
-##polypath
-grid.newpage()
-vp = plotViewport(mar, xscale = lim[1:2], yscale = lim[3:4])
-pushViewport(vp)
-n = length(out[,1])
-x = out[1:n,1]
-y = out[1:n,2]
-id = grid.id[1:n]
-a = grid.path(x, y, id=id, gp=gpar(fill="orange"), default.units="native", rule = 'winding')
--- standalone/peraxes.txt
+++
@@ -1,4 +0,0 @@
-lowest = function(y1, y2, y3, y4)
-{
-	(y1 <= y2) && (y1 <= y3) && (y1 <= y4)
-}
