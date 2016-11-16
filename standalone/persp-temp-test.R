90f087af3fccdfceddef75000ee420be96a42755
jason <374567155@qq.com>
2016-11-17 08:36:44 +1300

    updated

--- standalone/persp-temp-test.R
+++ standalone/persp-temp-test.R
@@ -1,6 +1,6 @@
 #setwd('H:/Documents/mproject/MasterProject-master')
 setwd('C:/Users/yeamin/Desktop/mproject/MasterProject/standalone/') ##at uni
-setwd('H:/Documents/MasterProject-master/MasterProject') ## at home
+setwd('C:/Users/yeamin/Desktop/master/MasterProject/MasterProject/standalone/') ## at home
 ##################################################################################
 
 
@@ -39,8 +39,3 @@ trans = persp(x, y, z, theta = 20, xlim = c(-5,5),
 plot = recordPlot()
 plotInfo = perInit(plot, trans, newpage = TRUE)
 per(plot = plotInfo)
-
-
-
-
-
